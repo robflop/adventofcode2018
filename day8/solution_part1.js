8dbc71b5df62c0a1d0cb33282bd0ecbf2af54eca
robflop <robflop98@gmail.com>
2018-12-08 11:44:22 +0100

    Solution for day 8 part 1 + example visualization

--- day8/solution_part1.js
+++ day8/solution_part1.js
@@ -1,11 +1,10 @@
-const input = require('fs').readFileSync('./input.1.txt', 'utf8').split(' ');
+const input = require('fs').readFileSync('./input.txt', 'utf8').split(' ');
 
 const metaStack = [];
 
 function getMetadata(length) {
 	for (let i = 0; i < length; i++) {
 		const meta = input.shift();
-		console.log('Added to metaStack:', meta);
 		metaStack.push(meta);
 	}
 }
@@ -13,21 +12,20 @@ function getMetadata(length) {
 function getChildren() {
 	const node = [input.shift(), input.shift()].map(n => parseInt(n));
 	// Node = [children, metadata];
-	console.log('Processing node:', node.join(' '));
+	let children = node[0];
 
 	if (node[0] > 0) {
-		for (let i = 0; i < node[0]; i++) {
+		for (; children > 0; children--) {
 			getChildren(node[0]);
 		}
+		if (children === 0) getMetadata(node[1]);
 	}
 	else {
-		console.log('Processing metadata of node:', node.join(' '));
 		getMetadata(node[1]);
 	}
 }
 
 getChildren();
-getMetadata(input.length);
 
 const metaSum = metaStack.map(m => parseInt(m)).reduce((stack, metadata) => stack = stack + metadata, 0);
 
