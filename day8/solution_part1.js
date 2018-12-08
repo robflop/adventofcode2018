6ceb764d9c9f12fa47c84ea51992ccb1228acfa5
robflop <robflop98@gmail.com>
2018-12-08 11:57:02 +0100

    Refactor day8 part1
    Move comment
    
    .

--- day8/solution_part1.js
+++ day8/solution_part1.js
@@ -1,32 +1,27 @@
-const input = require('fs').readFileSync('./input.txt', 'utf8').split(' ');
+const input = require('fs').readFileSync('./input.txt', 'utf8').split(' ').map(i => parseInt(i));
 
-const metaStack = [];
-
-function getMetadata(length) {
-	for (let i = 0; i < length; i++) {
-		const meta = input.shift();
-		metaStack.push(meta);
-	}
-}
-
-function getChildren() {
-	const node = [input.shift(), input.shift()].map(n => parseInt(n));
-	// Node = [children, metadata];
-	let children = node[0];
+function getChildren(metadataStack) {
+	const node = [input.shift(), input.shift()]; // [children, metadata];
 
 	if (node[0] > 0) {
-		for (; children > 0; children--) {
-			getChildren(node[0]);
+		for (; node[0] > 0; node[0]--) {
+			metadataStack = getChildren(metadataStack);
 		}
-		if (children === 0) getMetadata(node[1]);
+		if (node[0] === 0) metadataStack = getMetadata(node[1], metadataStack);
 	}
-	else {
-		getMetadata(node[1]);
-	}
+	else metadataStack = getMetadata(node[1], metadataStack);
+
+	return metadataStack;
 }
 
-getChildren();
+function getMetadata(length, metadataStack) {
+	for (let i = 0; i < length; i++) {
+		metadataStack.push(input.shift());
+	}
 
-const metaSum = metaStack.map(m => parseInt(m)).reduce((stack, metadata) => stack = stack + metadata, 0);
+	return metadataStack;
+}
+
+const metaSum = getChildren([]).reduce((stack, metadata) => stack = stack + metadata, 0);
 
 console.log('The resulting metadata sum is:', metaSum);
